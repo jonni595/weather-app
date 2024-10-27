7c6fecf8a399d6b0378096a961cc4096dceba08e
Jonathan <jonathandajome@gmail.com>
2024-10-27 13:05:25 -0500

    add general interface

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,3 +1,67 @@
+import { IconType } from "react-icons";
+
 export interface DateStampProps {
-  city: string;
+  city: string | undefined;
+}
+
+export interface IWeather {
+  base: string;
+  clouds: Clouds;
+  cod: number;
+  coord: Coord;
+  dt: number;
+  id: number;
+  main: Main;
+  name: string;
+  sys: Sys;
+  timezone: number;
+  visibility: number;
+  weather: Weather[];
+  wind: Wind;
+}
+
+export interface Clouds {
+  all: number;
+}
+
+export interface Coord {
+  lat: number;
+  lon: number;
+}
+
+export interface Main {
+  feels_like: number;
+  grnd_level: number;
+  humidity: number;
+  pressure: number;
+  sea_level: number;
+  temp: number;
+  temp_max: number;
+  temp_min: number;
+}
+
+export interface Sys {
+  country: string;
+  id: number;
+  sunrise: number;
+  sunset: number;
+  type: number;
+}
+
+export interface Weather {
+  description: string;
+  icon: string;
+  id: number;
+  main: string;
+}
+
+export interface Wind {
+  deg: number;
+  speed: number;
+}
+
+export interface Temp {
+  Icon: IconType;
+  temp: number | string | undefined;
+  description: string | undefined;
 }
