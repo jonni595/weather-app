40ccd79ca45adc308de49efcc3e77eacfa878c2d
Jonathan <jonathandajome@gmail.com>
2024-10-27 13:06:22 -0500

    add custom hook

---
+++ src/hooks/useWeather.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import type { IWeather } from "../interface";
+import { API_KEY } from "./../config";
+
+export const useWeather = (initialValue: string) => {
+  const [weather, setWeather] = useState<IWeather | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const getWeather = async () => {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${initialValue}&appid=${API_KEY}`
+      );
+      if (!res.ok) {
+        throw new Error("Failed to fetch weather data");
+      }
+      const data = await res.json();
+      setWeather(data);
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("An unexpected error occurred");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+  return {
+    weather,
+    error,
+    loading,
+  };
+};
