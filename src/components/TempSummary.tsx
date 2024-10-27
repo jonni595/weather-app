1b0d26f1c489347c052dd0e98fc5f67ea8cb2d65
Jonathan <jonathandajome@gmail.com>
2024-10-27 14:49:46 -0500

    add TempSummary component

---
+++ src/components/TempSummary.tsx
@@ -0,0 +1,22 @@
+import type { TempSummary } from "../interface";
+
+const TempSummary: React.FC<TempSummary> = ({ pressure, humidity, wind }) => {
+  return (
+    <div className="temp-summary">
+      <div className="pressure">
+        <p>Pressure </p>
+        <span>{pressure}</span>
+      </div>
+      <div className="humidity">
+        <p>Humidity </p>
+        <span>{humidity} %</span>
+      </div>
+      <div className="wind">
+        <p>Wind Speed </p>
+        <span>{wind} km/h</span>
+      </div>
+    </div>
+  );
+};
+
+export default TempSummary;
